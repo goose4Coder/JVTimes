98a5d0a1083a8a94e7f1ade97f04a70885d363ef
Aleksandr Basangov <aleksandrbasangov.1994@gmail.com>
2025-02-02 19:35:21 +0200

    feature:CategorySorting

--- src/articles/models.py
+++ src/articles/models.py
@@ -2,13 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_ckeditor_5.fields import CKEditor5Field
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+    title = models.CharField('Title',max_length=60)
+    def __str__(self):
+        return self.title
+
 class Article(models.Model):
     title = models.CharField('Title',max_length=60)
     description = models.CharField('Description',max_length=300, blank=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     creation_date = models.DateTimeField(auto_now=True, verbose_name="creation_date")
     content = CKEditor5Field('Content', config_name='default')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
     def __str__(self):
         return self.title
     @property
--- src/articles/urls.py
+++ src/articles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("all/", views.ArticleListView.as_view(), name='all-articles'),
+    path("of_category/<int:pk>/", views.ArticleByCategoryListView.as_view(),name='article-of-category'),
     path("read/<int:pk>/", views.ArticleDetailView.as_view(),name='read-article'),
+   
 ]
